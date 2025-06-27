30cd1f72bbd4540d637bde0e0da65254c9c139e4
Eduardo Rodrigues <arpegius2008@yahoo.com>
2025-06-27 11:09:09 +0200

    Revert "Refactor the code and create the NavigationApp."
    This reverts commit 6e9a4f3a4f88b700e527added1978a2c4da1c28c.

--- app/src/main/java/de/syntax_institut/androidabschlussprojekt/MainActivity.kt
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import de.syntax_institut.androidabschlussprojekt.ui.screens.HomeScreen
 import de.syntax_institut.androidabschlussprojekt.ui.theme.AndroidAbschlussprojektTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +17,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidAbschlussprojektTheme {
-                NavigationApp()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
+                }
             }
         }
     }
