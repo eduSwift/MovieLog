67788e4f271c409f2450ed3ac8646a71b19cbd17
Eduardo Rodrigues <arpegius2008@yahoo.com>
2025-07-08 12:46:41 +0200

    Revert "Revert "Add Auth Screen and create AuthViewModel.""
    This reverts commit 83521f48827b2f441c9a59db782f9fc77085ab2c.

--- app/src/main/java/de/syntax_institut/androidabschlussprojekt/ui/components/MainScaffold.kt
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/ui/components/MainScaffold.kt
@@ -29,7 +29,7 @@ fun MainScaffold(
     val items = listOf(
         Routes.HOME to Icons.Default.Home,
         Routes.SEARCH to Icons.Default.Search,
-        Routes.PROFILE to Icons.Default.Person
+        Routes.AUTH to Icons.Default.Person
     )
 
     Scaffold(
